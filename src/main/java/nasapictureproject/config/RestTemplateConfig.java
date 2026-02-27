4c88bd1f80e889c5c8803600edd05bf7f1986463
artem <artemkag04@gmail.com>
2026-02-27 22:20:03 +0300

    Initial commit

--- src/main/java/nasapictureproject/config/RestTemplateConfig.java
+++ src/main/java/nasapictureproject/config/RestTemplateConfig.java
@@ -12,5 +12,3 @@ public class RestTemplateConfig {
         return new RestTemplate();
     }
 }
-//TODO
-//возможно дело в том, что в apodResponse в json больше полей, возможно в том, что сервис упал у наса
